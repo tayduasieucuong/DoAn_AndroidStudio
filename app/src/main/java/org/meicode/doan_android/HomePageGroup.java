82a4a640e91e82b313ea856d54cd08a049c7788f
doanhnhantre <65936332+hieulechanhkk@users.noreply.github.com>
2021-12-21 19:03:31 +0700

    Update group first

---
+++ app/src/main/java/org/meicode/doan_android/HomePageGroup.java
@@ -0,0 +1,26 @@
+package org.meicode.doan_android;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePageGroup extends AppCompatActivity {
+    private void changeStatusBarColor(String color){
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor(color));
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_group);
+        changeStatusBarColor("#6D85F6");
+    }
+}
